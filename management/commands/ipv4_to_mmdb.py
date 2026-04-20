29a1fc071444835fd4d54262550b1cfe992ca0b4
Chris J. Karr <chris@audacious-software.com>
2026-04-20 04:58:49 +0000

    Improving read-only Python 2 support

--- management/commands/ipv4_to_mmdb.py
+++ management/commands/ipv4_to_mmdb.py
@@ -3,7 +3,7 @@
 from mmdb_writer import MMDBWriter
 from netaddr import IPSet
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand # pylint: disable=wrong-import-order
 
 class Command(BaseCommand):
     help = 'Converts an ipv4 text files to a mmdb file.'
@@ -26,14 +26,14 @@ class Command(BaseCommand):
             for line in ip_txt:
                 line = line.strip()
 
-                if len(line) > 0:
+                if len(line) > 0: # pylint: disable=len-as-condition
                     writer.insert_network(IPSet([line]), {'endpoint_type': 'Datacenter'})
 
         with open(options['vpn_list'], encoding='utf-8') as ip_txt:
             for line in ip_txt:
                 line = line.strip()
 
-                if len(line) > 0:
+                if len(line) > 0: # pylint: disable=len-as-condition
                     writer.insert_network(IPSet([line]), {'endpoint_type': 'VPN'})
 
         writer.to_db_file(options['destination'])
