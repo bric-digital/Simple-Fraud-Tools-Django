ec1fe5560b94a9823708cc8ea48e01851083d2fb
Chris J. Karr <chris@audacious-software.com>
2026-05-25 23:39:29 +0000

    Python 2 code cleanup

--- admin.py
+++ admin.py
@@ -2,18 +2,20 @@ from django.contrib import admin
 
 from .models import IPAddressDatabase
 
+def activate(modeladmin, request, queryset): # pylint: disable=unused-argument, no-self-use
+    queryset.update(active=True)
+
+activate.short_description = 'Activate selected IP address databases'
+
+def deactivate(modeladmin, request, queryset): # pylint: disable=unused-argument, no-self-use
+    queryset.update(active=False)
+
+deactivate.short_description = 'Deactivate selected IP address databases'
+
 @admin.register(IPAddressDatabase)
 class IPAddressDatabaseAdmin(admin.ModelAdmin):
     list_display = ('name', 'identifier', 'active', 'updated')
     search_fields = ('name', 'identifier', 'source',)
     list_filter = ('active', 'updated', 'added', 'identifier',)
 
-    actions = ['activate', 'deactivate']
-
-    @admin.action(description='Activate selected databases')
-    def activate(self, request, queryset): # pylint: disable=unused-argument
-        queryset.update(active=True)
-
-    @admin.action(description='Deactivate selected databases')
-    def deactivate(self, request, queryset): # pylint: disable=unused-argument
-        queryset.update(active=False)
+    actions = [activate, deactivate]
